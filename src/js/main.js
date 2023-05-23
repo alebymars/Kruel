95cefc480cf434b7a96938e729b2854d01cecca8
Alexandr Voronkov <voronkov@vk.com>
2023-05-23 14:39:53 +0700

    update main & auth styles

--- src/js/main.js
+++ src/js/main.js
@@ -72,7 +72,7 @@ const add = () => {
     emptyResponse.className = "plugCard";
     emptyResponse.id = "plugCard";
     emptyResponse.innerHTML =
-      "<p>Это некорректный запрос, попробуй что-то другое :)</p>";
+      "<p class='plugText'>Это некорректный запрос, попробуй что-то другое :)</p>";
     contentBlock.appendChild(emptyResponse);
     console.log("empty");
   }
